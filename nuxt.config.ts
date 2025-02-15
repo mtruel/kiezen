8ae7f72a187d67f223d043a8e25181827d93cbb0
Mathias Truel <28542694+mtruel@users.noreply.github.com>
2025-02-15 16:03:06 +0100

    Add new modules and cleanup configuration files
    Updated the Nuxt configuration to include additional modules such as `@vueuse/nuxt`, `@prisma/nuxt`, and `motion-v/nuxt`. Removed the redundant `tsconfig.json` file and cleaned up unused dependencies in `package-lock.json`. Added a `site.webmanifest` and `about.txt` for better app metadata.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  compatibilityDate: "2025-01-11"
+    devtools: {enabled: true},
+    modules: ["@nuxt/ui", "@vueuse/nuxt", "@prisma/nuxt", 'motion-v/nuxt'],
+    compatibilityDate: "2025-01-11",
 })
