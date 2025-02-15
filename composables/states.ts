ec756c09a4a91a88d6e1cbfc871d5efcd3f99cd8
Mathias Truel <28542694+mtruel@users.noreply.github.com>
2025-02-15 16:19:25 +0100

    Remove unused Prisma import and commented-out code
    The Prisma import was unused and has been removed to clean up the code. Additionally, a redundant commented-out line related to `usePlayingSong` was deleted for better readability and maintainability.

--- composables/states.ts
+++ composables/states.ts
@@ -1,6 +1,3 @@
-import {Prisma} from "@prisma/client";
-
-
 export const useAuthentication = () => useState<boolean>("authenticated", () => false);
 export const usePlayingSong = () => useState('playingSong', () => ({
     id: 0,
@@ -8,4 +5,3 @@ export const usePlayingSong = () => useState('playingSong', () => ({
     totalTime: 0,
     waveformPeaks: null,
 }));
-// export const usePlayingSong = () => useState<Number>('id', () => 0)
